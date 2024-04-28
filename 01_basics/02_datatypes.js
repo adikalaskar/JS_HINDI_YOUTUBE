800ba896a6681408df87fa570e86d07cd6cacb2f
Aditya Gangadhar Kalaskar <106766968+adikalaskar@users.noreply.github.com>
2024-04-28 10:13:47 +0000

    data_types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert("hello")  // we are  using nodejs , not browser
+
+console.log(3+3)
+
+
+let name = "Aditya"
+let age=18
+let is_logged_in = false
+
+
+// number =  2 to power 53
+// bigint 
+// string = ""
+// boolean= true , false   
+// null = standalone value ,representaiton of empty value
+// undefined = value not defined
+// symbol = to define unique 
+
+
+// object 
+
+console.log(typeof "Aditya");  // string
+console.log(typeof age);   // number
+console.log(typeof null); // object
+console.log(typeof undefined);  //  undefined
