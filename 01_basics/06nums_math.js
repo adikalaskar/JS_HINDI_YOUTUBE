9b7a069f048a27e8436aeb0227fb30e593818937
Aditya Gangadhar Kalaskar <106766968+adikalaskar@users.noreply.github.com>
2024-05-13 07:24:12 +0000

    Nums and maths

---
+++ 01_basics/06nums_math.js
@@ -0,0 +1,42 @@
+// // const score = 400
+// // console.log(score);
+// // const balance = new Number(100)
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+
+// // console.log(typeof(balance));
+
+
+// const otherNum  =  23.89656
+
+// console.log(otherNum.toPrecision(3));
+
+// const hundreds =  1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// *****************   MATHS ***********************
+
+// console.log(Math);
+// console.log(Math.abs(-4));    // ans is 4
+// console.log(Math.abs(4));    // ans is 4
+// console.log(Math.round(4.6));    // ans is 5
+// console.log(Math.ceil(4.2));    // ans is 5  upper value
+// console.log(Math.floor(4.9));    // ans is 4  lower value
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+
+console.log(Math.random());
+console.log(Math.random()* 10);
+console.log((Math.random()* 10)+1);
+console.log((Math.random()* 10)+1);
+console.log(Math.floor(Math.random()* 10)+1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1))+min);
