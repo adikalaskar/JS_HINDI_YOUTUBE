260126945527136239cd2de10034af0a53101cde
Aditya Gangadhar Kalaskar <106766968+adikalaskar@users.noreply.github.com>
2024-05-13 10:14:04 +0000

    dates

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());
+//console.log(myDate.toLocaleTimeString());
+
+//console.log(typeof myDate);
+
+// let myCreatedDate  =  new Date(2023 , 0 ,23)
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDateOF = new Date("01-14-2023")
+
+// console.log(myCreatedDateOF.toLocaleString());
+
+let myTimeStamp =  Date.now()
+console.log(myTimeStamp);
